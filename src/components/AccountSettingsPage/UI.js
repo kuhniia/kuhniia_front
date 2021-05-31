75d46cb9b70d5ffdf17cc869b346dabc1de55a74
Nadiia Fomenko <nfome@softserveinc.com>
2021-05-31 19:05:38 +0300

    [Settings page] implement comtainer pattern and add prop types validation

---
+++ src/components/AccountSettingsPage/UI.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { InputField, Spacer, Typography } from 'src/components/_shared';
+import { texts } from 'src/translations';
+import assets from 'src/assets';
+import { fonts, fontsSizes } from 'src/constants/fonts';
+
+import styles from './styles';
+
+const mock = {
+  name: 'Nadiia Fomenko',
+  date: 'joined 12 March, 2020',
+  email: 'nadiya.fomenko@gmail.com',
+  nickName: 'nadiya_fmnk',
+};
+
+const AccountSettingsPage = ({ selectPreloadPage = () => {} }) => {
+  return (
+    <ScrollView style={styles.root}>
+      <View style={styles.header}>
+        <Image source={assets.defaultAvatar} style={styles.avatar} />
+        <View>
+          <Typography font={fonts.robotoRegular} size={fontsSizes.H18}>
+            {mock.name}
+          </Typography>
+          <Typography font={fonts.robotoRegular} size={fontsSizes.H10}>
+            {mock.date}
+          </Typography>
+        </View>
+      </View>
+      <Spacer size={60} />
+      <InputField label={texts.email} iconSrc={assets.messageIcon} value={mock.email} readOnly />
+      <Spacer size={20} />
+      <InputField label={texts.username} iconSrc={assets.userIcon} value={mock.nickName} />
+      <Spacer size={60} />
+      <TouchableOpacity>
+        <Typography font={fonts.robotoRegular} size={fontsSizes.H14}>
+          {texts.changePassword}
+        </Typography>
+      </TouchableOpacity>
+      <Spacer size={30} />
+      <TouchableOpacity onPress={selectPreloadPage}>
+        <Typography font={fonts.robotoRegular} size={fontsSizes.H14}>
+          {texts.logOut}
+        </Typography>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+AccountSettingsPage.propTypes = {
+  selectPreloadPage: PropTypes.func,
+};
+
+export default AccountSettingsPage;
